04801cdc79ece91a8294743f9f865b422e90b20a
Keyvan Mir Mohammad Sadeghi <keyvan.m.sadeghi@gmail.com>
2013-09-13 21:33:27 +0430

    First draft

---
+++ link_grammar_web_ui/urls.py
@@ -0,0 +1,18 @@
+from django.conf.urls import patterns, include, url
+
+# Uncomment the next two lines to enable the admin:
+# from django.contrib import admin
+# admin.autodiscover()
+
+urlpatterns = patterns('',
+                       url(r'^$', 'parser_ui.views.index', name='index'),
+                       # Examples:
+                       # url(r'^$', 'link_grammar_web_ui.views.home', name='home'),
+                       # url(r'^link_grammar_web_ui/', include('link_grammar_web_ui.foo.urls')),
+
+                       # Uncomment the admin/doc line below to enable admin documentation:
+                       # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+                       # Uncomment the next line to enable the admin:
+                       # url(r'^admin/', include(admin.site.urls)),
+)
---
+++ parser_ui/views.py
@@ -0,0 +1,35 @@
+# Create your views here.
+from django.shortcuts import render_to_response
+from django.template import RequestContext
+from telnetlib import Telnet
+import json
+
+LG_SERVER = '127.0.0.1', 1234
+
+
+def index(request):
+    if request.method == 'POST':
+        sentence = str(request.POST.get('txt_sentence'))
+        tn = Telnet(*LG_SERVER)
+        tn.write('storeDiagramString:true,text:' + sentence + '\n')
+        parsed_value = ''
+        try:
+            parsed_value = tn.read_all()
+        except:
+            print 'error in reading telnet response...'
+            raise
+        tn.close()
+        lines = parsed_value.split("\n", 1)
+        parsed_value = lines[1]
+        parsed_value = json.loads(parsed_value)
+        parse_response = 'An error occurred while trying to parse the sentence...'
+        try:
+            parse_response = parsed_value['linkages'][0]['diagramString'] +\
+                             parsed_value['linkages'][0]['constituentString']
+        except:
+            print 'error in parsing JSON response...'
+            raise
+        return render_to_response('index.html',
+                                  {'show_result': True, 'sentence': sentence,
+                                   'parse_response': parse_response})
+    return render_to_response('index.html', context_instance=RequestContext(request))
