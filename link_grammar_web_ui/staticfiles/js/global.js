6b84aa55b618ae45487b097ce3d9d3b4ad102c3c
Keyvan Mir Mohammad Sadeghi <keyvan.m.sadeghi@gmail.com>
2013-09-17 01:35:21 +0430

    global.js

---
+++ link_grammar_web_ui/staticfiles/js/global.js
@@ -0,0 +1,34 @@
+/**
+ * Created with PyCharm.
+ * User: keyvan
+ * Date: 9/16/13
+ * Time: 3:57 PM
+ * To change this template use File | Settings | File Templates.
+ */
+
+
+function validate_number(evt) {
+    var e = evt || window.event;
+    var key = e.keyCode || e.which;
+
+    if (!e.shiftKey && !e.altKey && !e.ctrlKey &&
+    // numbers
+    key >= 48 && key <= 57 ||
+    // Numeric keypad
+    key >= 96 && key <= 105 ||
+    // Backspace and Tab and Enter
+    key == 8 || key == 9 || key == 13 ||
+    // Home and End
+    key == 35 || key == 36 ||
+    // left and right arrows
+    key == 37 || key == 39 ||
+    // Del and Ins
+    key == 46 || key == 45) {
+        // input is VALID
+    }
+    else {
+        // input is INVALID
+        e.returnValue = false;
+        if (e.preventDefault) e.preventDefault();
+    }
+}
